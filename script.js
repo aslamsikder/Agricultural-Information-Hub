0e0eaf7423a8d33f9cf97682bce7efa1dcb76887
Aslam Sikder <aslamsikderduet@gmail.com>
2024-11-23 17:03:15 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function doAlert() {
+    alert("Form submitted successfully!");
+}
+
+function hpAlert() {
+    alert("The Agricultural Hub page will be developed soon.");
+}
+
+function fgAlert() {
+    alert("The Farming Guide page will be developed soon.");
+}
+
+function cdAlert() {
+    alert("The Corp Details page will be developed soon.");
+}
+
+function pcAlert() {
+    alert("The Pest Control page will be developed soon.");
+}
